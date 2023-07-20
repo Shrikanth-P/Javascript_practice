b7096d1100b047c6dd241490e49230e39eee2052
Shrikanth P <99729339+Shrikanth-P@users.noreply.github.com>
2023-07-20 22:29:23 +0530

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,256 @@
+// Declaring different variables of different data types
+let firstName = 'Asabeneh' // first name of a person
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age = 100 // age in years
+let isRaining = true
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Asabeneh', 
+  job = 'teacher',
+  live = 'Finland'
+
+const PI = Math.PI
+console.log(PI) // 3.141592653589793
+console.log(Math.round(PI)) // 3; to round values to the nearest number
+console.log(Math.round(9.81)) // 10
+console.log(Math.floor(PI)) // 3; rounding down
+console.log(Math.ceil(PI)) // 4; rounding up
+console.log(Math.min(-5, 3, 20, 4, 5, 10)) // -5, returns the minimum value
+console.log(Math.max(-5, 3, 20, 4, 5, 10)) // 20, returns the maximum value
+
+const randNum = Math.random() // creates random number between 0 to 0.999999
+console.log(randNum)
+// Let create random number between 0 to 10
+const num = Math.floor(Math.random() * 11) // creates random number between 0 and 10
+console.log(num)
+
+//Absolute value
+console.log(Math.abs(-10)) //10
+//Square root
+console.log(Math.sqrt(100)) // 10
+console.log(Math.sqrt(2)) //1.4142135623730951
+// Power
+console.log(Math.pow(3, 2)) // 9
+
+
+let nums = [1, 2, 3]
+nums[0] = 10
+console.log(nums) // [10, 2, 3]
+
+let nums = [1, 2, 3]
+let numbers = [1, 2, 3]
+console.log(nums == numbers) // false
+
+let userOne = {
+  name: 'Asabeneh',
+  role: 'teaching',
+  country: 'Finland'
+}
+let userTwo = {
+  name: 'Asabeneh',
+  role: 'teaching',
+  country: 'Finland'
+}
+console.log(userOne == userTwo) // false
+
+let numbers = nums
+console.log(nums == numbers)  // true
+
+let userOne = {
+name:'Asabeneh',
+role:'teaching',
+country:'Finland'
+}
+let userTwo = userOne
+console.log(userOne == userTwo)  // true
+
+let a = 0
+if (a > 0) {
+  console.log(`${a} is a positive number`)
+} else if (a < 0) {
+  console.log(`${a} is a negative number`)
+} else if (a == 0) {
+  console.log(`${a} is zero`)
+} else {
+  console.log(`${a} is not a number`)
+}
+
+
+let weather = 'cloudy'
+switch (weather) {
+  case 'rainy':
+    console.log('You need a rain coat.')
+    break
+  case 'cloudy':
+    console.log('It might be cold, you need a jacket.')
+    break
+  case 'sunny':
+    console.log('Go out freely.')
+    break
+  default:
+    console.log(' No need for rain coat.')
+}
+
+
+for(let i = 0; i <= 5; i++){
+  console.log(i)
+}
+
+// 0 1 2 3 4 5
+
+const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+const newArr = []
+for(let i = 0; i < countries.length; i++){
+  newArr.push(countries[i].toUpperCase())
+}
+
+console.log(newArr)  // ["FINLAND", "SWEDEN", "NORWAY", "DENMARK", "ICELAND"]
+
+let fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.push('apple')
+console.log(fruits)    // ['banana', 'orange', 'mango', 'lemon', 'apple']
+
+fruits.push('lime')
+console.log(fruits)   // ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime']
+
+const numbers = [1, 2, 3, 4, 5]
+numbers.pop() // -> remove one item from the end
+console.log(numbers) // -> [1,2,3,4]
+
+const numbers = [1, 2, 3, 4, 5]
+numbers.reverse() // -> reverse array order
+console.log(numbers) // [5, 4, 3, 2, 1]
+
+let i = 0
+while (i <= 5) {
+  console.log(i)
+  i++
+}
+
+// 0 1 2 3 4 5
+
+let i = 0
+do {
+  console.log(i)
+  i++
+} while (i <= 5)
+
+// 0 1 2 3 4 5
+
+numbers = [1, 2, 3, 4, 5]
+
+for (const num of numbers) {
+  console.log(num)
+}
+
+// 1 2 3 4 5
+
+
+function areaOfCircle(r) {
+  let area = Math.PI * r * r
+  return area
+}
+
+console.log(areaOfCircle(10))
+
+function sumTwoNumbers(numOne, numTwo) {
+  let sum = numOne + numTwo
+  console.log(sum)
+}
+sumTwoNumbers(10, 20) 
+
+
+const sumAllNums = (...args) => {
+  let sum = 0
+  for (const element of args) {
+    sum += element
+  }
+  return sum
+}
+
+console.log(sumAllNums(1, 2, 3, 4)) // 10
+console.log(sumAllNums(10, 20, 13, 40, 10))  // 93
+console.log(sumAllNums(15, 20, 30, 25, 10, 33, 40))  // 173
+
+const generateFullName = (firstName = 'Asabeneh', lastName = 'Yetayeh') => {
+  let space = ' '
+  let fullName = firstName + space + lastName
+  return fullName
+}
+
+console.log(generateFullName())
+console.log(generateFullName('David', 'Smith'))
+
+
+const companies = ['Google', 'Facebook', 'Amazon', 'Oracle', 'Microsoft']
+setOfCompanies = new Set()
+for (const company of companies) {
+  setOfCompanies.add(company)
+}
+Set(5) {"Google", "Facebook", "Amazon", "Oracle", "Microsoft"}
+
+
+const languages = [
+  'English',
+  'Finnish',
+  'English',
+  'French',
+  'Spanish',
+  'English',
+  'French',
+]
+const langSet = new Set(languages)
+console.log(langSet) // Set(4) {"English", "Finnish", "French", "Spanish"}
+console.log(langSet.size) // 4
+
+const numbers = [5, 3, 2, 5, 5, 9, 4, 5]
+const setOfNumbers = new Set(numbers)
+
+console.log(setOfNumbers)
+//Set(5) {5, 3, 2, 9, 4}
+
+
+
+countries = [
+  ['Finland', 'Helsinki'],
+  ['Sweden', 'Stockholm'],
+  ['Norway', 'Oslo'],
+]
+const map = new Map(countries)
+console.log(map)
+console.log(map.size)
+//Map(3) {"Finland" => "Helsinki", "Sweden" => "Stockholm", "Norway" => "Oslo"}
+//3
+
+
+const countriesMap = new Map()
+console.log(countriesMap.size) // 0
+countriesMap.set('Finland', 'Helsinki')
+countriesMap.set('Sweden', 'Stockholm')
+countriesMap.set('Norway', 'Oslo')
+console.log(countriesMap)
+console.log(countriesMap.size)
+//Map(3) {"Finland" => "Helsinki", "Sweden" => "Stockholm", "Norway" => "Oslo"}
+//3
+
+
+
+class Person {
+  constructor(firstName, lastName) {
+    console.log(this) // Check the output from here
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+}
+
+const person = new Person()
+
+console.log(person)
+Person {firstName: undefined, lastName:undefined}
